f7ab6320fffd14bcb4baf560f338b4a9b0f510d2
BigDave <davidho104@gmail.com>
2018-09-22 13:59:33 -0500

    Updates

--- js.js
+++ js.js
@@ -1,34 +1,36 @@
 function buildQueryParams() {
-    
-    var queryParams = { "adults": "1" };
-    queryParams["cabinClass"] = "economy";
-    queryParams["children"] = "0";
-    queryParams["country"] = "US";
-    queryParams["currency"] = "USD";
-    queryParams["destinationPlace"] = "EWR-SKY";
-    queryParams["inboundDate"] = "2018-09-29";
-    queryParams["infants"] = "0";
-    queryParams["locale"] = "en-US";
-    queryParams["originPlace"] = "AUS-SKY";
-    queryParams["outboundDate"] = "2018-09-22";
 
-    return queryParams;
+    let queryParams = {
+        adults: "1",
+        cabinClass: "economy",
+        children: "0",
+        country: "US",
+        currency: "USD",
+        destinationPlace: "EWR-SKY",
+        inboundDate: "2018-09-29",
+        infants: "0",
+        locale: "en-US",
+        originPlace: "AUS-SKY",
+        outboundDate: "2018-09-22"
+    };
+    // return queryParams;
 }
-
 function getHeader() {
-    var queryHeaders = { "X-Mashape-Key": "LqjKeeIqzImshkHyYu0Rk9eT1LxSp1obbmljsnuDOes4Umc5Dt" };
-    queryHeaders["Content-Type"] = "application/x-www-form-urlencoded";
-    queryHeaders["Accept"] = "application/json";
-    queryHeaders["Access-Control-Allow-Origin"] = "*";
-    // queryHeaders["Access-Control-Allow-Methods"] = "POST";
-    // queryHeaders["Access-Control-Allow-Headers"] = "Content-Type, Authorization";
-
-    return queryHeaders;
+    let queryHeaders = {
+        "X-Mashape-Key": "LqjKeeIqzImshkHyYu0Rk9eT1LxSp1obbmljsnuDOes4Umc5Dt",
+        "X-Mashape-Host": "skyscanner-skyscanner-flight-search-v1.p.mashape.com",
+        "Content-Type": "application/x-www-form-urlencoded",
+        "Accept": "application/json",
+        "Access-Control-Allow-Origin": "*",
+        // "Access-Control-Allow-Methods": "POST",
+        "Access-Control-Allow-Headers": "Content-Type, Authorization"
+    };
+    // return queryHeaders;
 }
-
 function createSession() {
+    let location;
     var queryURL = "https://skyscanner-skyscanner-flight-search-v1.p.mashape.com/apiservices/pricing/v1.0";
-    var r = $.ajax({
+    $.ajax({
         url: queryURL,
         headers: getHeader(),
         // data: JSON.stringify(user)
@@ -36,18 +38,18 @@ function createSession() {
         // dataType: 'jsonp',
         // cache: true,
         // crossDomain: true,
-        data: buildQueryParams(),
-        success: function(response){
-            // debugger
-            console.log(response);
-        },error: function(response){
-            console.log("Error:"+ response);
-            // debugger;
-        }
-    });
-
+        data: buildQueryParams()
+    }).then((data, status, xhr) => {
+        location = xhr.getResponseHeader();
+    })
+    return location;
+    // return createSession();
 }
+// , error: function(response) {
+    // console.log("Error:" + response);
+    // debugger;
 
 function getPrices() {
     createSession();
+    // return getPrices();
 }
