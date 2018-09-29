053cc345a2cf8eb0ed9389cf0f4597a726ca78ee
kathdovi <36436123+kathdovi@users.noreply.github.com>
2018-09-29 00:41:50 -0500

    Update my-trips.js

--- my-trips.js
+++ my-trips.js
@@ -7,12 +7,12 @@ database.ref(`users/${userID}/trips`).on("child_added", function (snapshot) {
     let personCity1 = snapshot.val().person1
     let personCity2 = snapshot.val().person2
     let finalDest = snapshot.val().location
-    // let totalCost = snapshat.val().money
+    let totalCost = snapshat.val().money
 
     $("#cityState1").html(`${personCity1}`);
     $("#cityState2").html(`${personCity2}`);
     $("#finalDestination").html(`${finalDest}`);
-    // $("#totalPrice").html(`${totalCost}`);
+    $("#totalPrice").html(`${totalCost}`);
 });
 
 
